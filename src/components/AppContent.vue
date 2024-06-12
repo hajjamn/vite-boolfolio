d80004a3c7b6c8c8046a7928a53b6cc4693bc272
hajjamn <hajjamn@gmail.com>
2024-06-12 17:11:32 +0200

    ADD: started implementing pagination

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,11 +1,27 @@
 <script>
+import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
   components: {
+    ProjectCard
   },
   data() {
     return {
+      currentPageProjects: []
     }
+  },
+  methods: {
+    fetchProject() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+        .then((response) => {
+          console.log(response.data.results.data)
+          this.currentPageProjects = response.data.results.data
+        })
+    }
+  },
+  created() {
+    this.fetchProject()
   }
 }
 
@@ -14,14 +30,23 @@ export default {
 <template>
 
   <main>
-    <section class="h-100">
-      <div class="container py-5 h-100">
-        <div class="row aling-items-center flex-column justify-content-center h-100">
-          <div class="col-auto text-center">
-            <h1>Hello Vue <font-awesome-icon :icon="['fab', 'vuejs']" /></h1>
+    <section>
+      <div class="container py-5">
+        <div class="row">
+          <div v-for="project in currentPageProjects" class="col-4 p-3">
+            <ProjectCard :item="project"></ProjectCard>
           </div>
         </div>
       </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-auto">
+            <span class="fs-4">1</span>
+          </div>
+        </div>
+      </div>
+
     </section>
   </main>
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+    item: Object
+  },
+  craeted() {
+    console.log(item)
+  }
+}
+
+</script>
+
+<template>
+
+  <div class="card h-100">
+    <!-- HEADER -->
+    <div class="card-header text-center">
+      <h3 class="fs-5">{{ item.title }}</h3>
+    </div>
+    <!-- BODY -->
+    <div class="card-body">
+      <ul class="list-group mb-4">
+        <li class="list-group-item"><a href="#">{{ item.repo }}</a></li>
+        <li class="list-group-item">{{ item.type_id }}</li>
+      </ul>
+      <p>{{ item.description }}</p>
+    </div>
+    <!-- FOOTER -->
+    <div class="card-footer">
+      <div class="row">
+        <div class="col-auto rounded-circle bg-secondary border border-black">
+          <span class="fs-4">{{ item.id }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style></style>
